f7b9e9a40ab211bb6c97b0d32d7a79ce3639700c
rmkrishna91 <mohanakrishnareddi@gmail.com>
2025-03-16 11:42:49 +0530

    solved numerical_df issue

--- ai_gen_ui.py
+++ ai_gen_ui.py
@@ -272,7 +272,7 @@ scaler.mean_ = np.load("scaler_mean.npy")  # Load mean
 scaler.scale_ = np.load("scaler_scale.npy")  # Load scale
 
 # Load Sentence Transformer model
-numerical_features = torch.tensor(scaler.transform(numerical_df), dtype=torch.float32).to(device)
+# numerical_features = torch.tensor(scaler.transform(numerical_df), dtype=torch.float32).to(device)
 
 # List of required numerical features (must match training order)
 FEATURE_COLUMNS = [
